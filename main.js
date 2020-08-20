1d2976b89885c417e0355d6077614ce04c7cced0
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-08-20 11:58:50 -0400

    not done

--- main.js
+++ main.js
@@ -4,9 +4,50 @@ const makeRunOn = (sentence) => {
 return sentence.split('. ').join(', ');
 }
 
-const totalScore = () => {
+const totalScore = (allScores) => {
+  //adds all scores
+  //levels.score should === 1000
+  //if multiplier === !1
+
+return allScores.score = 1000
+}
+
+const nightOwls = () => {
   
 }
+
+const getToBed = () => {
+
+}
+
+const findIndices = (arr, callBack) => {
+  //
+
+}
+
+const Faqtory = () => {
+  return {
+    questions: [],
+    
+    addQuestion (text, answered = false){
+      this.questions.push({
+        text: text,
+        answered: answered,
+        id: this.questions.length
+      });
+    },
+    
+    answerQuestion(givenId, answer = true){
+      if (this.questions[givenId].answered === false)
+      this.questions[givenId].answer = answer;
+
+      if (this.questions[givenId].answered === true) {
+//unfinished
+      }
+    } 
+  }
+}
+
 
 
 
